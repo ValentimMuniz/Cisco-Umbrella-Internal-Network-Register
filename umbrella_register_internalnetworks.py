2a13fd56f15240d05b095cf32db59075789b136f
Valentim Uliana <valentim.muniz@hotmail.com>
2020-11-03 15:31:39 -0300

    Update umbrella_register_internalnetworks.py

--- umbrella_register_internalnetworks.py
+++ umbrella_register_internalnetworks.py
@@ -11,7 +11,7 @@
 #    GNU General Public License for more details.
  
 #    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/# Copyright (C) 2019  Alexandre Argeris
+#    along with this program.  If not, see <https://www.gnu.org/licenses/# Copyright (C) 2020 Valentim Uliana
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
